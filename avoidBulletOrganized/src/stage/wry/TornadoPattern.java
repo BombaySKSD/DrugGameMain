4f2e2aa8b70e596080ddf7785ca26578ddedbd78
Lee Jeong Heon <kormckill@naver.com>
2014-06-05 21:25:01 +0900

    회오으리 데미지 빼고 완성

--- avoidBulletOrganized/src/stage/wry/TornadoPattern.java
+++ avoidBulletOrganized/src/stage/wry/TornadoPattern.java
@@ -12,7 +12,7 @@ public class TornadoPattern extends Pattern {
 	private static Random rand=new Random();
 	
 	private WryStage stage;
-	private int where;
+	private int where,xvel;
 	
 	public TornadoPattern(WryStage stage){
 		this.stage=stage;
@@ -21,12 +21,16 @@ public class TornadoPattern extends Pattern {
 	
 	@Override
 	public SingleObject create() {
+		where+=xvel;
+		xvel+=rand.nextBoolean()?3:-3;
+		xvel*=Math.abs(xvel)>10?0.8:2;
+		xvel*=0<where && where<stage.getWidth()?1:-1;
 		return new TornadoPart(where,stage.getHeight()-90,rand.nextInt(8));
 	}
 
 	@Override
 	public boolean createWhen() {
-		return stage.count%5==0;
+		return stage.count%15==0 && (6<stage.second() && stage.second()<8);
 	}
 
 	@Override
@@ -49,25 +53,31 @@ public class TornadoPattern extends Pattern {
 
 class TornadoPart extends SingleObject{
 
-	private final static int numImage=8;
-	private static Image[] imageSet=new Image[numImage];
+	private final static int numImage=8,numSize=50;
+	private final static int initialWidth=50,initialHeight=25,diff=4;
+	private static Image[][] imageSet=new Image[numImage][numSize];
 	
-	private int whichImage;
+	private int whichImage,whichSize;
 	
 	static{
 		for(int i=0;i<numImage;i++){
-			imageSet[i]=getImage("tornado/tornado"+(i+1)+".png");
+			Image tmp=getImage("tornado/tornado"+(i+1)+".png");
+			for(int j=0;j<numSize;j++){
+				imageSet[i][j]=tmp.getScaledInstance(initialWidth+j*diff*2, initialHeight+j*diff, Image.SCALE_FAST);
+			}
 		}
 	}
 	public TornadoPart(double x, double y,int whichImage) {
-		super(x, y, Math.PI, 5, imageSet[whichImage]);
+		super(x, y, Math.PI, 1, imageSet[whichImage][0]);
 		this.whichImage=whichImage;
+		whichSize=0;
 	}
 	
 	@Override
 	public void move() {
 		super.move();
-		image=imageSet[(++whichImage)%numImage];
+		image=imageSet[(++whichImage/6)%numImage][(++whichSize/10)%numSize];
+		x-=image.getWidth(null)/200;
 	}
 	
 	private static Image getImage(String path){
