2c69b0ee46b05ef823fb25bcebacf49cf8adb09c
Lee Jeong Heon <kormckill@naver.com>
2014-06-11 16:56:54 +0900

    스타트화면 추가중

---
+++ avoidBulletOrganized/src/stage/start/StartPage.java
@@ -0,0 +1,32 @@
+package stage.start;
+
+import object.Stage;
+
+public class StartPage extends Stage{
+
+	private Image background=getImage("")
+	
+	@Override
+	public void init() {
+		
+	}
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean continuing() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
